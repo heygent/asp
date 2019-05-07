3ca7f69f4cf57e4abbbdfd3901157fb0c863f261
heygent <heygent@users.noreply.github.com>
2019-05-07 09:57:07 +0200

    può fare il sommario di più answer set; rompe tsv

--- calendario.py
+++ calendario.py
@@ -9,65 +9,17 @@ Richiede la libreria `tabulate` (in Ubuntu: `sudo apt install python-tabulate`)
 import re
 import sys
 import argparse
-from enum import IntEnum
-from collections import namedtuple
 from itertools import groupby
 from tabulate import tabulate
-
-SYMBOL_RE = r"(?P<{}>\w[\d\w]*)"
-NUMBER_RE = r"(?P<{}>\d+)"
-STRING_RE = r'"(?P<{}>[^"]*)"'
-
-
-class PredicateParser:
-    def __init__(self, name, arglist):
-        self.arglist = arglist
-        args_re = r',\s*'.join(
-            regex.format(name) for name, regex, *_ in arglist)
-        self.re = re.compile(rf"{name}\({args_re}\)")
-        self.result_tuple = namedtuple(name,
-                                       [argname for argname, *_ in arglist])
-
-    def parse(self, input_str):
-        for match in self.re.finditer(input_str):
-            parsed_args = match.groupdict()
-            for argname, _, argtype in self.arglist:
-                parsed_args[argname] = argtype(parsed_args[argname])
-
-            yield self.result_tuple(**parsed_args)
-
-
-class GiornoSettimana(IntEnum):
-    lunedi = 1
-    martedi = 2
-    mercoledi = 3
-    giovedi = 4
-    venerdi = 5
-
-    def __str__(self):
-        return self.name
-
-
-ORARIO_PARSER = PredicateParser('orario', [
-    ('classe', SYMBOL_RE, str),
-    ('giorno', SYMBOL_RE, GiornoSettimana.__getitem__),
-    ('ora', NUMBER_RE, int),
-    ('materia', SYMBOL_RE, str),
-    ('aula', SYMBOL_RE, str),
-])
-
-CLASSE_HA_DOCENTE_PARSER = PredicateParser('classe_ha_docente', [
-    ('classe', SYMBOL_RE, str),
-    ('materia', SYMBOL_RE, str),
-    ('docente', STRING_RE, str),
-])
-
+from collections import defaultdict
+from lib.calendario_output_parser import parse_clingo_output
 
 def make_classe_table_dict(righe_orario, attr_names):
-    orario = { giorno: [] for giorno in GiornoSettimana }
+    orario = defaultdict(list)
 
     for riga_orario in righe_orario:
-        data = map(lambda attrname: str(getattr(riga_orario, attrname)), attr_names)
+        data = map(lambda attrname: str(getattr(riga_orario, attrname)),
+                   attr_names)
         orario[riga_orario.giorno].append('\n\n'.join(data))
 
     return orario
@@ -88,7 +40,10 @@ def make_docenti_table_dict(pred_tuples):
     return docenti, materie
 
 
-def print_summary(righe, docenti, cell_data):
+def print_summary(model, cell_data):
+    righe, docenti, number = model
+    print(f'Answer Set: {number}')
+
     for classe, righe_classe in groupby(righe, lambda r: r.classe):
         orario_classe = make_classe_table_dict(righe_classe, cell_data)
 
@@ -122,17 +77,11 @@ if __name__ == '__main__':
     parser.add_argument('--cell-data', action=Split, default=['materia', 'aula'])
     ns = parser.parse_args()
 
-    with ns.infile:
-        input_str = ns.infile.read()
-
-    righe = sorted(ORARIO_PARSER.parse(input_str))
-    docenti = sorted(CLASSE_HA_DOCENTE_PARSER.parse(input_str))
-
-    if ns.tsv:
-        print('\t'.join(ORARIO_PARSER.result_tuple._fields))
-        for riga in righe:
-            print('\t'.join(map(str, riga)))
-    else:
-        if ns.clingo_output:
-            print(input_str)
-        print_summary(righe, docenti, ns.cell_data)
+    with ns.infile as infile:
+        for model in parse_clingo_output(infile):
+            if ns.tsv:
+                print('\t'.join(ORARIO_PARSER.result_tuple._fields))
+                for riga in righe:
+                    print('\t'.join(map(str, riga)))
+            else:
+                print_summary(model, ns.cell_data)
---
+++ lib/calendario_output_parser.py
@@ -0,0 +1,77 @@
+import re
+from collections import namedtuple
+from enum import IntEnum
+from itertools import tee
+
+SYMBOL_RE = r"(?P<{}>\w[\d\w]*)"
+NUMBER_RE = r"(?P<{}>\d+)"
+STRING_RE = r'"(?P<{}>[^"]*)"'
+
+
+class PredicateParser:
+    def __init__(self, name, arglist):
+        self.arglist = arglist
+        args_re = r',\s*'.join(
+            regex.format(name) for name, regex, *_ in arglist)
+        self.re = re.compile(rf"{name}\({args_re}\)")
+        self.result_tuple = namedtuple(name,
+                                       [argname for argname, *_ in arglist])
+
+    def parse(self, input_str):
+        for match in self.re.finditer(input_str):
+            parsed_args = match.groupdict()
+            for argname, _, argtype in self.arglist:
+                parsed_args[argname] = argtype(parsed_args[argname])
+
+            yield self.result_tuple(**parsed_args)
+
+
+class GiornoSettimana(IntEnum):
+    lunedi = 1
+    martedi = 2
+    mercoledi = 3
+    giovedi = 4
+    venerdi = 5
+
+    def __str__(self):
+        return self.name
+
+
+ORARIO_PARSER = PredicateParser('orario', [
+    ('classe', SYMBOL_RE, str),
+    ('giorno', SYMBOL_RE, GiornoSettimana.__getitem__),
+    ('ora', NUMBER_RE, int),
+    ('materia', SYMBOL_RE, str),
+    ('aula', SYMBOL_RE, str),
+])
+
+CLASSE_HA_DOCENTE_PARSER = PredicateParser('classe_ha_docente', [
+    ('classe', SYMBOL_RE, str),
+    ('materia', SYMBOL_RE, str),
+    ('docente', STRING_RE, str),
+])
+
+calendario_model = namedtuple('calendario_model',
+                              'orario classe_ha_docente number')
+
+
+def pairwise(iter):
+    a, b = tee(iter)
+    next(b)
+    return zip(a, b)
+
+
+ANSWER_RE = re.compile(r'Answer: (\d+)')
+
+
+def parse_clingo_output(line_iter):
+    for answer_str, model_str in pairwise(line_iter):
+        answer_match = ANSWER_RE.match(answer_str)
+        if answer_match:
+            model_number = answer_match.group(1)
+            yield calendario_model(
+                number=model_number,
+                orario=sorted(ORARIO_PARSER.parse(model_str)),
+                classe_ha_docente=sorted(
+                    CLASSE_HA_DOCENTE_PARSER.parse(model_str)),
+            )
